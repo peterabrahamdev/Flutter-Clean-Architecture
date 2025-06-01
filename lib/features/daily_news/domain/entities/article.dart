cca2f6df767ba785e3eca2445c119f8ab6937675
Peter Abraham <peterabrahamdev@gmail.com>
2025-06-01 11:58:41 +0200

    feat: create ArticleEntity class

---
+++ lib/features/daily_news/domain/entities/article.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+
+// When you make a class extend Equatable, it allows you to compare two objects based on the values inside them, not just where they are stored in memory.
+class ArticleEntity extends Equatable {
+  final int? id;
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+
+  const ArticleEntity({
+    this.id,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  @override
+  List<Object?> get props => [
+    id,
+    author,
+    title,
+    description,
+    url,
+    urlToImage,
+    publishedAt,
+    content,
+  ];
+}
