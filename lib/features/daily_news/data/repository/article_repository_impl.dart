0477a97d2dcf77ea115277dcbad156edbb527cfe
Peter Abraham <peterabrahamdev@gmail.com>
2025-06-02 18:06:22 +0200

    feat: create ArticleRepositoryImpl class

---
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_clean_architecture/core/resources/data_state.dart';
+import 'package:flutter_clean_architecture/features/daily_news/data/models/article.dart';
+import 'package:flutter_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository{
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticles() {
+    // TODO: implement getNewsArticles
+    throw UnimplementedError();
+  }
+
+  
+}
