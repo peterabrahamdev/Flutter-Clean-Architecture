4ad51590d9f6e566dc284202109fed3d4a4dfeb4
Peter Abraham <peterabrahamdev@gmail.com>
2025-06-02 20:06:38 +0200

    feat: add fetching to ArticleRepositoryImpl

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,13 +1,41 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_architecture/core/constants/constants.dart';
 import 'package:flutter_clean_architecture/core/resources/data_state.dart';
+import 'package:flutter_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:flutter_clean_architecture/features/daily_news/data/models/article.dart';
 import 'package:flutter_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
+import 'package:retrofit/retrofit.dart';
 
-class ArticleRepositoryImpl implements ArticleRepository{
+class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+  ArticleRepositoryImpl(this._newsApiService);
+
+  // Method to fetch news articles from the remote API.
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
-  }
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsApiKey,
+        category: categoryQuery,
+        country: countryQuery,
+      );
 
-  
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioException(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions,
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
 }
