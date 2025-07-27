28177e450b6f62feaddfd47c970b8d62c37888e3
Peter Abraham <peterabrahamdev@gmail.com>
2025-07-27 16:43:30 +0200

    feat: implement bloc

--- lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -2,11 +2,11 @@ import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_clean_architecture/features/daily_news/domain/entities/article.dart';
 
-abstract class RemoteArticleState extends Equatable{
+abstract class RemoteArticlesState extends Equatable{
   final List<ArticleEntity>? articles;
   final DioException? dioException;
 
-  const RemoteArticleState({this.articles, this.dioException});
+  const RemoteArticlesState({this.articles, this.dioException});
 
   // The props getter allows comparison between state instances for equality. 
   // Note: The ! asserts these fields are non-null. This could throw errors if either is actually null.
@@ -14,14 +14,14 @@ abstract class RemoteArticleState extends Equatable{
   List<Object> get props => [articles!, dioException!];
 }
 
-class RemoteArticlesLoading extends RemoteArticleState {
+class RemoteArticlesLoading extends RemoteArticlesState {
   const RemoteArticlesLoading();
 }
 
-class RemoteArticlesDone extends RemoteArticleState {
+class RemoteArticlesDone extends RemoteArticlesState {
   const RemoteArticlesDone(List<ArticleEntity> article) : super(articles: article) ;
 }
 
-class RemoteArticlesError extends RemoteArticleState {
+class RemoteArticlesError extends RemoteArticlesState {
   const RemoteArticlesError(DioException dioException) : super (dioException: dioException);
 }
